1825dc202699be988611a9fc74e234103e5aab58
intopiece <if-ok@163.com>
2021-07-15 11:26:58 +0800

    fix: 修复safari首次打印pdf空白

--- src/js/print.js
+++ src/js/print.js
@@ -1,102 +1,108 @@
-import Browser from './browser'
-import { cleanUp } from './functions'
+import Browser from './browser';
+import { cleanUp } from './functions';
 
 const Print = {
   send: (params, printFrame) => {
     // Append iframe element to document body
-    document.getElementsByTagName('body')[0].appendChild(printFrame)
+    document.getElementsByTagName('body')[0].appendChild(printFrame);
 
     // Get iframe element
-    const iframeElement = document.getElementById(params.frameId)
+    const iframeElement = document.getElementById(params.frameId);
 
     // Wait for iframe to load all content
     iframeElement.onload = () => {
       if (params.type === 'pdf') {
         // Add a delay for Firefox. In my tests, 1000ms was sufficient but 100ms was not
-        if (Browser.isFirefox()) {
-          setTimeout(() => performPrint(iframeElement, params), 1000)
+        if (Browser.isFirefox() || Browser.isSafari()) {
+          setTimeout(() => performPrint(iframeElement, params), 1000);
         } else {
-          performPrint(iframeElement, params)
+          performPrint(iframeElement, params);
         }
-        return
+        return;
       }
 
       // Get iframe element document
-      let printDocument = (iframeElement.contentWindow || iframeElement.contentDocument)
-      if (printDocument.document) printDocument = printDocument.document
+      let printDocument =
+        iframeElement.contentWindow || iframeElement.contentDocument;
+      if (printDocument.document) printDocument = printDocument.document;
 
       // Append printable element to the iframe body
-      printDocument.body.appendChild(params.printableElement)
+      printDocument.body.appendChild(params.printableElement);
 
       // Add custom style
       if (params.type !== 'pdf' && params.style) {
         // Create style element
-        const style = document.createElement('style')
-        style.innerHTML = params.style
+        const style = document.createElement('style');
+        style.innerHTML = params.style;
 
         // Append style element to iframe's head
-        printDocument.head.appendChild(style)
+        printDocument.head.appendChild(style);
       }
 
       // If printing images, wait for them to load inside the iframe
-      const images = printDocument.getElementsByTagName('img')
+      const images = printDocument.getElementsByTagName('img');
 
       if (images.length > 0) {
-        loadIframeImages(Array.from(images)).then(() => performPrint(iframeElement, params))
+        loadIframeImages(Array.from(images)).then(() =>
+          performPrint(iframeElement, params)
+        );
       } else {
-        performPrint(iframeElement, params)
+        performPrint(iframeElement, params);
       }
-    }
-  }
-}
+    };
+  },
+};
 
-function performPrint (iframeElement, params) {
+function performPrint(iframeElement, params) {
   try {
-    iframeElement.focus()
+    iframeElement.focus();
 
     // If Edge or IE, try catch with execCommand
     if (Browser.isEdge() || Browser.isIE()) {
       try {
-        iframeElement.contentWindow.document.execCommand('print', false, null)
+        iframeElement.contentWindow.document.execCommand('print', false, null);
       } catch (e) {
-        iframeElement.contentWindow.print()
+        iframeElement.contentWindow.print();
       }
     } else {
       // Other browsers
-      iframeElement.contentWindow.print()
+      iframeElement.contentWindow.print();
     }
   } catch (error) {
-    params.onError(error)
+    params.onError(error);
   } finally {
     if (Browser.isFirefox()) {
       // Move the iframe element off-screen and make it invisible
-      iframeElement.style.visibility = 'hidden'
-      iframeElement.style.left = '-1px'
+      iframeElement.style.visibility = 'hidden';
+      iframeElement.style.left = '-1px';
     }
 
-    cleanUp(params)
+    cleanUp(params);
   }
 }
 
-function loadIframeImages (images) {
-  const promises = images.map(image => {
+function loadIframeImages(images) {
+  const promises = images.map((image) => {
     if (image.src && image.src !== window.location.href) {
-      return loadIframeImage(image)
+      return loadIframeImage(image);
     }
-  })
+  });
 
-  return Promise.all(promises)
+  return Promise.all(promises);
 }
 
-function loadIframeImage (image) {
-  return new Promise(resolve => {
+function loadIframeImage(image) {
+  return new Promise((resolve) => {
     const pollImage = () => {
-      !image || typeof image.naturalWidth === 'undefined' || image.naturalWidth === 0 || !image.complete
+      !image ||
+      typeof image.naturalWidth === 'undefined' ||
+      image.naturalWidth === 0 ||
+      !image.complete
         ? setTimeout(pollImage, 500)
-        : resolve()
-    }
-    pollImage()
-  })
+        : resolve();
+    };
+    pollImage();
+  });
 }
 
-export default Print
+export default Print;
